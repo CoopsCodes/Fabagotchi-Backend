4061cb833b31885bae81a69e7aeb7f96142d5915
Cooper Viktor <im.cooperviktor@gmail.com>
2019-08-14 18:44:46 +1000

    server and mongoose DB setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const server = express();
+const PORT = 8000;
+
+// const routes = require("./routes");
+const databaseUri = process.env.ATLAS_URI;
+
+mongoose.connect(databaseUri, { useNewUrlParser: true }, err => {
+  if (err) console.log(err);
+  console.log("Connected to MongoDB");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
