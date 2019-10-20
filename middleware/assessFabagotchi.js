35e832968ea2f9ee7df70ddca4e43c61da11dba3
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-10-20 20:31:54 +1100

    Stub out middleware

---
+++ middleware/assessFabagotchi.js
@@ -0,0 +1,5 @@
+// This will be middleware to assess the state of a Fabagotchi when it is 'woken up'.
+
+const assessFabagotchi = (req, res, next) => {
+    throw new Error("Not yet implemented")
+}
